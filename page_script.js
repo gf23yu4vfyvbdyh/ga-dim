702520902952416ce8b837f84290dccd2f6e6927
gf23yu4vfyvbdyh <okada_yu@micware.co.jp>
2024-06-18 15:16:32 +0900

    Add files via upload

--- page_script.js
+++ page_script.js
@@ -1,8 +1,7 @@
-function butotnClick2(){
-    alert('ボタンがクリックされました２');
+function butotnClick2() {
+    console.log("ボタンがクリックされました２")
 }
 
 let button2 = document.getElementById('btn_1');
 console.log("テスト用")
-// addEventListener( 'イベント', 処理)で要素にイベントが発火した際に処理を実行する
 button2.addEventListener('click', butotnClick2);
