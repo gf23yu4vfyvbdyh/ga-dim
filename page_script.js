576f71e18e291b0983a878b5140b4d2ea0eded6f
gf23yu4vfyvbdyh <okada_yu@micware.co.jp>
2024-06-18 13:57:54 +0900

    Update page_script.js

--- page_script.js
+++ page_script.js
@@ -1,7 +1,8 @@
-
-function butotnClick1(){
-    alert('ボタンがクリックされました');
+function butotnClick2(){
+    alert('ボタンがクリックされました２');
 }
 
-let button1 = document.getElementById('btn_1');
-button1.onclick = butotnClick1;
+let button2 = document.getElementById('btn_1');
+console.log("テスト用")
+// addEventListener( 'イベント', 処理)で要素にイベントが発火した際に処理を実行する
+button2.addEventListener('click', butotnClick2);
