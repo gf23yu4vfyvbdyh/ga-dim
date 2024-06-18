b6c93f6c6ec0e939b75d6b0eb3b2e258199c7dfc
gf23yu4vfyvbdyh <okada_yu@micware.co.jp>
2024-06-18 16:12:38 +0900

    Update page_script.js

--- page_script.js
+++ page_script.js
@@ -5,3 +5,6 @@ function butotnClick2() {
 let button2 = document.getElementById('btn_1');
 console.log("テスト用")
 button2.addEventListener('click', butotnClick2);
+gtag('config', 'G-ERHTPZWRQ3', {
+    'user_id': 'USER_ID_test5000000'
+});
