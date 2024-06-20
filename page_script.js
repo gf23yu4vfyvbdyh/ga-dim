bd86e0af2da6119a65f9c04e72d0a8061a1f4dde
gf23yu4vfyvbdyh <okada_yu@micware.co.jp>
2024-06-20 19:02:46 +0900

    Update page_script.js

--- page_script.js
+++ page_script.js
@@ -1,5 +1,12 @@
 function butotnClick2() {
     console.log("ボタンがクリックされました２")
+
+    gtag('set', { 'user_id': 'USER_ID_test240620' })
+    var random_lat = Math.floor(Math.random() * 100) + 150;
+    var random_lng = Math.floor(Math.random() * 100) + 150;
+
+    gtag('set', { 'usrt_lat': 'latnum' + random_lat })
+    gtag('set', { 'user_lng': 'lngnim' + random_lng })
 }
 
 let button2 = document.getElementById('btn_1');
@@ -8,3 +15,7 @@ button2.addEventListener('click', butotnClick2);
 gtag('config', 'G-ERHTPZWRQ3', {
     'user_id': 'USER_ID_test5000000'
 });
+
+var random_lat = Math.floor(Math.random() * 100) + 150;
+var random_lng = Math.floor(Math.random() * 100) + 150;
+gtag('set', { 'user_id': 'USER_ID_testP240620', 'usrt_lat': 'Platnum' + random_lat, 'user_lng': 'Plngnim' + random_lng })
